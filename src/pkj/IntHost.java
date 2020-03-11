ff82c1d13dcac8c4334f204318005d8219ebd1fd
Justi <Justi@Alpha-RX>
2020-03-11 17:01:52 -0400

    test code updated

--- src/pkj/IntHost.java
+++ src/pkj/IntHost.java
@@ -19,6 +19,15 @@ public class IntHost
 	static final String INTHOST = "Intermediate Server";
 	public static final int port = 23;
 
+	private int sampleTime = 20000;
+	private ThreadMXBean threadMxBean = ManagementFactory.getThreadMXBean();
+	private RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
+	private OperatingSystemMXBean osMxBean = ManagementFactory
+			.getOperatingSystemMXBean();
+	private Map<Long, Long> threadInitialCPU = new HashMap<Long, Long>();
+	private Map<Long, Float> threadCPUUsage = new HashMap<Long, Float>();
+	private long initialUptime = runtimeMxBean.getUptime();
+
 	/**
 	 * constructor
 	 */
@@ -65,16 +74,66 @@ public class IntHost
 			System.exit(1);
 		}
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IntHost i = new IntHost();
+		
 		while(true) {
 			i.reply();
+			//new IntHost().measure();
 		}
+		
+	}
+	
+	private void measure() {
+		 
+		ThreadInfo[] threadInfos = threadMxBean.dumpAllThreads(false, false);
+		for (ThreadInfo info : threadInfos) {
+			threadInitialCPU.put(info.getThreadId(),
+					threadMxBean.getThreadCpuTime(info.getThreadId()));
+		}
+ 
+		try {
+			Thread.sleep(sampleTime);
+		} catch (InterruptedException e) {
+		}
+ 
+		long upTime = runtimeMxBean.getUptime();
+ 
+		Map<Long, Long> threadCurrentCPU = new HashMap<Long, Long>();
+		threadInfos = threadMxBean.dumpAllThreads(false, false);
+		for (ThreadInfo info : threadInfos) {
+			threadCurrentCPU.put(info.getThreadId(),
+					threadMxBean.getThreadCpuTime(info.getThreadId()));
+		}
+ 
+		// CPU over all processes
+		int nrCPUs = osMxBean.getAvailableProcessors();
+		// total CPU: CPU % can be more than 100% (devided over multiple cpus)
+		//long nrCPUs = 1;
+		// elapsedTime is in ms.
+		long elapsedTime = (upTime - initialUptime);
+		for (ThreadInfo info : threadInfos) {
+			// elapsedCpu is in ns
+			Long initialCPU = threadInitialCPU.get(info.getThreadId());
+			if (initialCPU != null) {
+				long elapsedCpu = threadCurrentCPU.get(info.getThreadId())
+						- initialCPU;
+				float cpuUsage = elapsedCpu * 100/ (elapsedTime * 1000000F * nrCPUs);
+				threadCPUUsage.put(info.getThreadId(), cpuUsage);
+			}
+		}
+ 
+		// threadCPUUsage contains cpu % per thread
+		System.out.println(threadCPUUsage);
+		// You can use osMxBean.getThreadInfo(theadId) to get information on
+		// every thread reported in threadCPUUsage and analyze the most CPU
+		// intentive threads
+ 
 	}
 
 }
